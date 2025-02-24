683e2e1b7b03218a7c13966ea5c773dc28633412
7zism3 <7zism3@gmail.com>
2025-02-24 20:40:42 +0700

    init

---
+++ src/main/java/com/sevenhallo/tourbackend/TourBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sevenhallo.tourbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourBackendApplication.class, args);
+	}
+
+}
