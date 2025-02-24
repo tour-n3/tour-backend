1d6a68a33b29d9a23b22e5cdebd92596f47ac3e9
7zism3 <7zism3@gmail.com>
2025-02-24 21:46:07 +0700

    init

---
+++ src/test/java/com/sevenhallo/tourbackend/TourBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevenhallo.tourbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
