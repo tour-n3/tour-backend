f90d60214ef30b22289ee4976debfcb20ed78d09
7zism3 <7zism3@gmail.com>
2025-02-25 19:18:27 +0700

    Revert "init - test"
    This reverts commit ab4f47b4c4f1b4fc404b4c16e98bf560f4c592bf.

--- src/main/java/com/sevenhallo/tourbackend/TourBackendApplication.java
+++ src/main/java/com/sevenhallo/tourbackend/TourBackendApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TourBackendApplication {
 
 	public static void main(String[] args) {
-		System.out.println("DB_URL: " + System.getenv("DB_URL"));
-		System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
-		System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
-
-
 		SpringApplication.run(TourBackendApplication.class, args);
 	}
 
